753b600d9f632427c091edb00ed51548d5b5bdbe
Jethro <ttppoo12144@gmail.com>
2025-01-04 23:46:47 +1100

    新增 首頁 文字

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>首頁</div>;
+  return (
+    <div className="container flex h-screen items-center justify-center">
+      <h1 className="text-5xl">學習React TypeScript</h1>
+    </div>
+  );
 };
 
 export default Home;
