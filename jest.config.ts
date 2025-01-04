6f0cf3ce5d8f258be36d6a8324c81b5b48f86745
Jethro <ttppoo12144@gmail.com>
2025-01-05 01:29:58 +1100

    測試 jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
---
+++ jest.setup.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder as UtilTextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = UtilTextDecoder as unknown as {
+  new (label?: string, options?: TextDecoderOptions): TextDecoder;
+  prototype: TextDecoder;
+};
