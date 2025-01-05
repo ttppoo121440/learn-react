dceaa2d6f3cf3d0e1ba5d4904ba58ba1401f8fa7
Jethro <ttppoo12144@gmail.com>
2025-01-05 23:43:17 +1100

    正確配置 jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
@@ -6,7 +8,9 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/test/__mocks__/fileMock.js',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+
+export default config;
