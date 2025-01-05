7680dffcdf03bcfe394187b761053dcef096fd6e
Jethro <ttppoo12144@gmail.com>
2025-01-05 23:43:30 +1100

    新增 env 的問題

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
@@ -7,6 +7,16 @@ import path from 'path';
  * @see https://vitejs.dev/config/
  */
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        ['process.env.' + key]: `"${val}"`,
+      };
+    },
+    {},
+  );
   return {
     base: mode === 'development' ? '/' : '/learn-react/',
     plugins: [react(), eslintPlugin()],
@@ -18,5 +28,6 @@ export default defineConfig(({ mode }) => {
     server: {
       open: 'index.html',
     },
+    define: envWithProcessPrefix,
   };
 });
