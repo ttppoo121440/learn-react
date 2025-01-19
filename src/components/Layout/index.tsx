c1d86d503f95108c2d71f1e22f465069333ca757
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:27:28 +1100

    新增 dark 模式

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,19 @@
 import { ProviderProps } from '@/types/ProviderType';
-import Navbar from './Navbar';
+
+import { ThemeProvider } from '../theme-provider';
+import { Toaster } from '../ui/toaster';
+
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Layout = ({ children }: ProviderProps) => {
   return (
     <>
       <Navbar />
-      <div className="flex-1">{children}</div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="flex-1 ">{children}</div>
+      </ThemeProvider>
+      <Toaster />
       <Footer />
     </>
   );
