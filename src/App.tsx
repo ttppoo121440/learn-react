ff35ec063f9f00064a8da1df2dd31fc2007efa23
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:21:35 +1100

    新增 AppRoutes 負責管理router 把app抽離出來 乾淨點

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { HashRouter, Route, Routes } from 'react-router';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Week1 from './pages/Week1';
-import Week2 from './pages/Week2';
+import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
-  return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/week1" element={<Week1 />} />
-          <Route path="/week2" element={<Week2 />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </Layout>
-    </HashRouter>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,34 @@
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import Layout from '@/components/Layout';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import Week1 from '@/pages/Week1';
+import Week2 from '@/pages/Week2';
+import Week3 from '@/pages/Week3';
+import ProductManagement from '@/pages/Week3/components/ProductManagement';
+
+import PrivateRoute from './PrivateRoute';
+
+const AppRoutes = () => {
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/week1" element={<Week1 />} />
+          <Route path="/week2" element={<Week2 />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/week3" element={<Week3 />}>
+              <Route path="product-management" element={<ProductManagement />} />
+              <Route index element={<Navigate to="product-management" />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
+};
+
+export default AppRoutes;
