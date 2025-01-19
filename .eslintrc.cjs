4df96a6652325160aadcb7420ba0b6db4cc4ccf0
Jethro <ttppoo12144@gmail.com>
2025-01-19 13:36:55 +1100

    修正 eslint 規則

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,6 @@
+const { type } = require('os');
+const { default: path } = require('path');
+
 module.exports = {
   root: true,
   env: {
@@ -9,6 +12,16 @@ module.exports = {
     react: {
       version: 'detect',
     },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,14 +30,7 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    'prettier',
-    '@typescript-eslint',
-    'react-refresh',
-    'import',
-  ],
+  plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint', 'react-refresh', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -34,7 +40,68 @@ module.exports = {
     'plugin:prettier/recommended',
     'eslint-config-prettier',
   ],
-  rules: {},
+  rules: {
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': [
+      'error',
+      {
+        fixToUnknown: false,
+        ignoreRestArgs: false,
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'import/default': 'error',
+    'import/namespace': 'error',
+    'import/no-duplicates': 'error',
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        json: 'always',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+          },
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
   ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs', 'eslint.config.js'],
   globals: {
     Edit: 'writable',
