89dde1f8fdd77a2f021bab6aa42d50aecbe1bbd4
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:29:56 +1100

    新增 產品相關API

--- src/api/services/product/index.ts
+++ src/api/services/product/index.ts
@@ -1,11 +1,38 @@
 import axiosClient from '@/api/axiosClient';
-import { ProductResponse } from './types';
+import { ProductQueryResponseSchema } from '@/schema/productSchema';
+import { safeParseResponse } from '@/utils/zodUtils';
+
+import { BaseProductApiType, ProductQueryParamsType, ProductResponseType } from './types';
 
 export const productApi = {
-  all: async () => {
-    const response = await axiosClient.get<ProductResponse>(
-      `/api/${process.env.VITE_BASE_PATH}/products/all`,
-    );
+  baseUrl: `/api/${process.env.VITE_BASE_PATH}/admin`,
+  all: async ({ page, category }: ProductQueryParamsType) => {
+    const response = await axiosClient.get<ProductResponseType>(`${productApi.baseUrl}/products`, {
+      params: { page, category },
+    });
+    const result = safeParseResponse(ProductQueryResponseSchema, response.data);
+    console.log('DTO API:', result);
+    return result;
+  },
+  create: async (product: BaseProductApiType) => {
+    const data = {
+      data: product,
+    };
+    const response = await axiosClient.post(`${productApi.baseUrl}/product`, data);
+    console.log(response.data);
+
+    return response.data;
+  },
+  update: async (id: string, product: BaseProductApiType) => {
+    const data = {
+      data: product,
+    };
+    const response = await axiosClient.put(`${productApi.baseUrl}/product/${id}`, data);
+    console.log(response.data);
+    return response.data;
+  },
+  delete: async (id: string) => {
+    const response = await axiosClient.delete(`${productApi.baseUrl}/product/${id}`);
     return response.data;
   },
 };
