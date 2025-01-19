1f7a0c06bc2ee885dedca0c78ac2bbca3fd94930
Jethro <ttppoo12144@gmail.com>
2025-01-19 13:39:45 +1100

    新增 QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
+
 import App from '@/App';
 import '@/index.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
+const queryClient = new QueryClient();
+
 root.render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>,
 );
