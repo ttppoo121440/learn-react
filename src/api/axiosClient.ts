56ee9d9b1dee6b0dc314a53c44453fe09b4b197c
Jethro <ttppoo12144@gmail.com>
2025-01-19 13:40:27 +1100

    新增 錯誤處理

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,19 +1,38 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import cookies from 'js-cookie';
 
-const userCookie = cookies.get('token');
+const userCookie = cookies.get('react-token');
 
-let token = '';
-if (userCookie) {
-  const user = JSON.parse(userCookie);
-  token = user.token;
-}
 const axiosClient = axios.create({
   baseURL: process.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `${token}`,
+    ...(userCookie && { Authorization: userCookie }),
   },
 });
+
+const goTOLogin = (error: AxiosError) => {
+  const status = error?.response?.status || null;
+  if (status === 401) {
+    if (typeof window !== 'undefined') {
+      window.location.href = '/#login';
+    }
+  }
+};
+
+axiosClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response) {
+      goTOLogin(error);
+      return Promise.reject(error.response.data);
+    } else {
+      goTOLogin(error);
+      return Promise.reject(error);
+    }
+  },
+);
 
 export default axiosClient;
