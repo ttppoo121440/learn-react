6611f3aed6dbaee160105cb463fb017a7b04acf4
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:31:23 +1100

    格式優化

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
-import { defineConfig, loadEnv } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import eslintPlugin from 'vite-plugin-eslint';
 
 /**
  * @see https://vitejs.dev/config/
  */
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const envWithProcessPrefix = Object.entries(env).reduce(
-    (prev, [key, val]) => {
-      return {
-        ...prev,
-        ['process.env.' + key]: `"${val}"`,
-      };
-    },
-    {},
-  );
+  const envWithProcessPrefix = Object.entries(env).reduce((prev, [key, val]) => {
+    return {
+      ...prev,
+      ['process.env.' + key]: `"${val}"`,
+    };
+  }, {});
   return {
     base: mode === 'development' ? '/' : '/learn-react/',
     plugins: [react(), eslintPlugin(), EnvironmentPlugin('all')],
