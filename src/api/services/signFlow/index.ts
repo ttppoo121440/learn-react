e929400fc87359e9d13900bcf7a9ed1adee967ae
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:29:04 +1100

    新增 logout

--- src/api/services/signFlow/index.ts
+++ src/api/services/signFlow/index.ts
@@ -1,5 +1,6 @@
-import { AuthResponse, FormLogin, LoginResponse } from './types';
 import axiosClient from '@/api/axiosClient';
+
+import { AuthResponse, FormLogin, LoginResponse } from './types';
 
 export const signFlowApi = {
   isCheckAuth: async () => {
@@ -7,10 +8,11 @@ export const signFlowApi = {
     return response.data;
   },
   signIn: async (user: FormLogin) => {
-    const response = await axiosClient.post<LoginResponse>(
-      `admin/signin`,
-      user,
-    );
+    const response = await axiosClient.post<LoginResponse>(`admin/signin`, user);
+    return response.data;
+  },
+  logout: async () => {
+    const response = await axiosClient.post(`/logout`);
     return response.data;
   },
 };
