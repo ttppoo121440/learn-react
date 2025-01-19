d60af579b5c796d3de4be654860893768ef09e3f
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:27:41 +1100

    把icon抽離出來使用

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,10 @@
+import { Pencil1Icon } from '@radix-ui/react-icons';
+import { Delete, SquareTerminal } from 'lucide-react';
+
+const Icons = {
+  Delete: Delete,
+  Edit: Pencil1Icon,
+  Dashboard: SquareTerminal,
+};
+
+export default Icons;
