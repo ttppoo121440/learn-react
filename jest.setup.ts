64f561ed75599db1a72e6dee841ab14a8415acb0
Jethro <ttppoo12144@gmail.com>
2025-01-19 17:03:52 +1100

    新增 eslint 無視 any

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,7 @@
-import '@testing-library/jest-dom'; // 確保引入 jest-dom
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder as UtilTextDecoder } from 'util';
 
-// 確保 global 可用於 Jest
 (global as any).TextEncoder = TextEncoder;
 (global as any).TextDecoder = UtilTextDecoder as unknown as {
   new (label?: string, options?: TextDecoderOptions): TextDecoder;
