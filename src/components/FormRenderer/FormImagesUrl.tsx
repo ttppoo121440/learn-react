423e1579606d6e5dd5b67ee4eb3714e017e3052e
Jethro <ttppoo12144@gmail.com>
2025-01-30 21:32:44 +1100

    修正 上傳圖片 元件

--- src/components/FormRenderer/FormImagesUrl.tsx
+++ src/components/FormRenderer/FormImagesUrl.tsx
@@ -1,37 +1,119 @@
-import { FieldValues, useFieldArray, useFormContext, Path } from 'react-hook-form';
+import { X } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { FieldValues, useFieldArray, useFormContext, ArrayPath, Path, PathValue } from 'react-hook-form';
 
+import { Button } from '../ui/button';
+import { FormLabel } from '../ui/form';
 import { Input } from '../ui/input';
 
-import { FormImagesInputProps } from './types';
-
-const FormImagesInput = <T extends FieldValues>({ name, maxCount = 5 }: FormImagesInputProps<T>) => {
-  const { control, register } = useFormContext<T>();
+const FormImagesInput = <T extends FieldValues>({
+  label,
+  name,
+  maxCount = 5,
+  onFileUpload,
+}: {
+  label: string;
+  name: ArrayPath<T>;
+  maxCount?: number;
+  onFileUpload?: (file: File) => void;
+}) => {
+  const { control, watch, setValue } = useFormContext<T>();
   const { fields, append, remove } = useFieldArray({
     control,
     name,
   });
 
+  const [previews, setPreviews] = useState<string[]>([]);
+
+  const fieldValues = watch(name as Path<T>);
+
+  const handleFileInputAdd = () => {
+    if (fields.length < maxCount) {
+      append('' as T[typeof name][string]);
+    }
+  };
+
+  const handleFileChange = (index: number) => async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.type.startsWith('image/')) {
+      const previewUrl = URL.createObjectURL(file);
+
+      const newPreviews = [...previews];
+      newPreviews[index] = previewUrl;
+      setPreviews(newPreviews);
+
+      const updatedFieldValues = [...(fieldValues || [])] as (typeof fieldValues)[];
+      updatedFieldValues[index] = file as (typeof fieldValues)[number];
+      setValue(name as Path<T>, updatedFieldValues as unknown as PathValue<T, Path<T>>);
+
+      if (onFileUpload) {
+        try {
+          await onFileUpload(file);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  };
+
+  const handleRemove = (index: number) => {
+    remove(index);
+
+    const newPreviews = [...previews];
+    newPreviews.splice(index, 1);
+    setPreviews(newPreviews);
+  };
+
+  useEffect(() => {
+    const urls = (fieldValues || []).map((value) =>
+      typeof value === 'string' ? value : URL.createObjectURL(value as File),
+    );
+    setPreviews(urls);
+
+    return () => {
+      urls.forEach((url) => {
+        if (url.startsWith('blob:')) {
+          URL.revokeObjectURL(url);
+        }
+      });
+    };
+  }, [fieldValues]);
+
   return (
-    <>
-      {fields.map((field, index) => (
-        <div key={field.id} className="flex items-center">
-          <Input
-            type="text"
-            {...register(`${name}.${index}` as Path<T>)}
-            placeholder={`圖片網址 ${index + 1}`}
-            className="grow"
-          />
-          <button type="button" onClick={() => remove(index)} className="ml-2 shrink-0 text-red-500">
-            移除
-          </button>
-        </div>
-      ))}
+    <div>
+      <FormLabel>{label}</FormLabel>
+      <div>
+        {fields.map((field, index) => (
+          <div key={field.id} className="relative">
+            <Input type="file" onChange={handleFileChange(index)} accept="image/*" />
+            {previews[index] && (
+              <div className="mb-5">
+                <div className="flex justify-center">
+                  <div className="relative mt-5 flex size-full justify-center overflow-hidden rounded-lg border border-gray-300">
+                    <img src={previews[index]} alt="Preview" className="object-cover" />
+                  </div>
+                </div>
+                <Button
+                  type="button"
+                  size="icon"
+                  variant="destructive"
+                  className="absolute right-1 top-1 h-6 w-6 rounded-full"
+                  onClick={() => handleRemove(index)}
+                >
+                  <X className="h-4 w-4" />
+                </Button>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
       {fields.length < maxCount && (
-        <button type="button" onClick={() => append('' as T[typeof name][string])} className="mt-2 text-primary">
-          新增圖片
-        </button>
+        <Button type="button" variant="outline" onClick={handleFileInputAdd} className="mt-2">
+          新增檔案
+        </Button>
       )}
-    </>
+    </div>
   );
 };
 
