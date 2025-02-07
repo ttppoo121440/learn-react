46730e245f094d15e705dacf678ea0a0ebfd304a
Jethro <ttppoo12144@gmail.com>
2025-02-07 12:04:11 +1100

    新增 前台 產品 Hook

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,98 +1,30 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
-import { ProductAdapter } from '@/api/adapters/ProductAdapter';
-import { productApi } from '@/api/services/product';
-import { ProductQueryParamsType, ProductVoType } from '@/api/services/product/types';
-import { MutationResult } from '@/types/mutationTypes';
-
-import { useToast } from './use-toast';
+import { ProductQueryParamsType } from '@/api/services/adminProductApi/types';
+import { productApi } from '@/api/services/productApi';
 
 const productKeys = {
-  key: ['UserNews'] as const,
+  key: ['product'] as const,
 };
-
-const productAdapter = new ProductAdapter();
 
 export const useGetProducts = (queryParams: ProductQueryParamsType) => {
   return useQuery({
     queryKey: [productKeys.key, queryParams],
     queryFn: async () => {
       const response = await productApi.all(queryParams);
-      const adaptedProducts = response.products.map((product) => productAdapter.dtoToVo(product));
-      console.log('adaptedProducts:', adaptedProducts);
-      return {
-        ...response,
-        products: adaptedProducts,
-      };
+      console.log('response:', response);
+      return response;
     },
   });
 };
 
-export const usePostProductMutation = (): MutationResult<ProductVoType> => {
-  const queryClient = useQueryClient();
-  const { toast } = useToast();
-  return useMutation({
-    mutationFn: async (newProduct: ProductVoType) => {
-      const dtoProduct = productAdapter.voToDto(newProduct);
-      return productApi.create(dtoProduct);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [productKeys.key] });
-      toast({
-        description: '新增成功!',
-      });
-    },
-    onError: (error) => {
-      console.error('Error:', error);
-      toast({
-        variant: 'destructive',
-        description: error.message,
-      });
-    },
-  });
-};
-
-export const useUpdateProductMutation = (): MutationResult<ProductVoType> => {
-  const queryClient = useQueryClient();
-  const { toast } = useToast();
-  return useMutation({
-    mutationFn: async (newProduct: ProductVoType) => {
-      const dtoProduct = productAdapter.voToDto(newProduct);
-      return productApi.update(dtoProduct.id, dtoProduct);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [productKeys.key] });
-      toast({
-        description: '修改成功!',
-      });
-    },
-    onError: (error) => {
-      console.error('Error:', error);
-      toast({
-        variant: 'destructive',
-        description: error.message,
-      });
-    },
-  });
-};
-
-export const useDeleteProductMutation = (): MutationResult<ProductVoType['id']> => {
-  const queryClient = useQueryClient();
-  const { toast } = useToast();
-  return useMutation({
-    mutationFn: productApi.delete,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [productKeys.key] });
-      toast({
-        description: '刪除成功!',
-      });
-    },
-    onError: (error) => {
-      console.error('Error:', error);
-      toast({
-        variant: 'destructive',
-        description: error.message,
-      });
+export const useGetProductId = (id: string) => {
+  return useQuery({
+    queryKey: [productKeys.key],
+    queryFn: async () => {
+      const response = await productApi.getById(id);
+      console.log('response:', response);
+      return response.product;
     },
   });
 };
