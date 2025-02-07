c0b62b27e2d460993e01e1f9d4ac266c486819a1
Jethro <ttppoo12144@gmail.com>
2025-02-07 12:04:54 +1100

    新增 第五周

---
+++ src/pages/Week5/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router';
+
+const Week5 = () => {
+  return (
+    <div className="bg-black">
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Week5;
