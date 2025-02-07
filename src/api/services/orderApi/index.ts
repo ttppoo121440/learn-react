7d89f5472f90f608f53c0182b6f699e86b62fe2b
Jethro <ttppoo12144@gmail.com>
2025-02-07 19:00:44 +1100

    修正 訂單 表單送出格式

--- src/api/services/orderApi/index.ts
+++ src/api/services/orderApi/index.ts
@@ -6,7 +6,10 @@ export const orderApi = {
   baseUrl: `/api/${process.env.VITE_BASE_PATH}/order`,
   create: async (orderData: orderPostType) => {
     const data = {
-      data: orderData,
+      data: {
+        user: orderData,
+        message: orderData.message,
+      },
     };
     const response = await axiosClient.post(`${orderApi.baseUrl}`, data);
     console.log(response.data);
