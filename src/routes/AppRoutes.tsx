577d0e88558fcaeeb8d87a94ac78c59e909ed35a
Jethro <ttppoo12144@gmail.com>
2025-02-07 19:02:40 +1100

    新增 第五周 頁面

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,6 +8,11 @@ import Week1 from '@/pages/Week1';
 import Week2 from '@/pages/Week2';
 import Week3 from '@/pages/Week3';
 import Week4 from '@/pages/Week4';
+import Week5 from '@/pages/Week5';
+import Checkout from '@/pages/Week5/Checkout';
+import DetailProduct from '@/pages/Week5/DetailProduct';
+import ProductsList from '@/pages/Week5/ProductsList';
+import ShoppingCart from '@/pages/Week5/ShoppingCart';
 
 import PrivateRoute from './PrivateRoute';
 
@@ -29,6 +34,13 @@ const AppRoutes = () => {
               <Route path="products" element={<ProductManagement />} />
               <Route index element={<Navigate to="products" />} />
             </Route>
+          </Route>
+          <Route path="/week5" element={<Week5 />}>
+            <Route index element={<Navigate to="ProductsList" />} />
+            <Route path="ProductsList" element={<ProductsList />} />
+            <Route path="ShoppingCart" element={<ShoppingCart />} />
+            <Route path="Checkout" element={<Checkout />} />
+            <Route path="ProductsList/DetailProduct/:id" element={<DetailProduct />} />
           </Route>
         </Routes>
       </Layout>
