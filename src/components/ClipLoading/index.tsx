e5323d80f9cf09acc0396ee34166ef1a1deffc56
Jethro <ttppoo12144@gmail.com>
2025-02-07 12:01:34 +1100

    新增 全局loading 方法

--- src/components/ClipLoading/index.tsx
+++ src/components/ClipLoading/index.tsx
@@ -2,11 +2,11 @@ import ClipLoader from 'react-spinners/ClipLoader';
 
 import { ClipLoadingProps } from './types';
 
-const ClipLoading = ({ loading }: ClipLoadingProps) => {
+const ClipLoading = ({ loading, global = false }: ClipLoadingProps) => {
   return (
-    <>
-      <ClipLoader color="#ffffff" loading={loading} aria-label="Loading Spinner" data-testid="loader" />
-    </>
+    <div className={global ? 'fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50' : ''}>
+      <ClipLoader color="#ffffff" loading={loading} size={global ? 100 : 20} aria-label="Loading Spinner" />
+    </div>
   );
 };
 
